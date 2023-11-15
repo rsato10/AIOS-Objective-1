eda7097a66f05186b24aca5f6695135b548f865b
Rob s <rsato@mail.sfsu.edu>
2023-11-15 01:37:57 -0800

    finished obj 1

--- app.py
+++ app.py
@@ -7,15 +7,27 @@ from apikey import apikey
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain
 
+#LLM
 os.environ['OPENAI_API_KEY'] = apikey
+llm = OpenAI(temperature = 0.9)
 
 #Framework
 st.title('Afterflea aios Objective 1')
-prompt = st.text_input('Enter prompt')
+with st.form("my_form"):
+    prompt_list = []
 
-#LLM
-llm = OpenAI(temperature = 0.9)
+    prompt_list.append(st.text_input('situation:'))
 
-if(prompt):
-    response = llm(prompt)
-    st.write(response)
+    prompt_list.append(st.text_input('emotion:'))
+
+    prompt_list.append(st.text_input('resolution:'))
+
+    prompt_list.append(st.text_area('Fill in with more details, please.'))
+
+    input = ' '.join(prompt_list)
+    
+    submit = st.form_submit_button()
+
+    if(input != ' ' and submit):
+        response = llm(input)
+        st.write(response)
